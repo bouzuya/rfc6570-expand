983d1a1f23f545006e4492974248c586fef0d438
bouzuya <m@bouzuya.net>
2016-05-30 01:45:54 +0900

    add level 2 support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
+type Operator = {
+  key: string;
+  first: string;
+  allow: (s: string) => string;
+};
 
 const u = (s: string): string => {
   return encodeURIComponent(s).replace(/[!'()*]/g, (c) => {
     return '%' + c.charCodeAt(0).toString(16);
   });
+};
+
+const ur = (s: string): string => {
+  return encodeURI(s).replace(/%5B/g, '[').replace(/%5D/g, ']');
+};
+
+const parseOperator = (expression: string): Operator => {
+  const operators = [
+    { key: '+', first: '', allow: ur },
+    // { key: '.' },
+    // { key: '/' },
+    // { key: ';' },
+    // { key: '?' },
+    // { key: '&' },
+    { key: '#', first: '#', allow: ur }
+  ];
+  const operator = operators.find(o => o.key === expression.charAt(1));
+  return operator ? operator : { key: null, first: '', allow: u };
 };
 
 const init = (template: string): { expand: (variables: any) => string; } => {
@@ -26,9 +49,10 @@ const init = (template: string): { expand: (variables: any) => string; } => {
       }
 
       const expression = template.slice(openIndex, closeIndex + 1);
-      const varName = expression.slice(1, expression.length - 1);
+      const { key, first, allow } = parseOperator(expression);
+      const varName = expression.slice((key ? 2 : 1), expression.length - 1);
       const value = variables[varName];
-      result.push(u(value));
+      result.push(first + allow(value));
 
       index = closeIndex + 1;
     }
