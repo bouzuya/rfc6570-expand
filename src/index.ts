10614d11e4e904fff7dc57fb62fd163bc1c007c6
bouzuya <m@bouzuya.net>
2016-05-30 07:57:40 +0900

    add level 3 support

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 type Operator = {
   key: string;
   first: string;
+  sep: string;
+  named: boolean;
+  ifemp: string;
   allow: (s: string) => string;
 };
 
@@ -15,25 +18,34 @@ const ur = (s: string): string => {
 };
 
 const parseOperator = (expression: string): Operator => {
-  const operators = [
-    { key: '+', first: '', allow: ur },
-    // { key: '.' },
-    // { key: '/' },
-    // { key: ';' },
-    // { key: '?' },
-    // { key: '&' },
-    { key: '#', first: '#', allow: ur }
+  const defaultOperator: Operator = {
+    key: null, first: '', sep: ',', named: false, ifemp: '', allow: u
+  };
+  const operators: Operator[] = [
+    { key: '+', first: '', sep: ',', named: false, ifemp: '', allow: ur },
+    { key: '.', first: '.', sep: '.', named: false, ifemp: '', allow: u },
+    { key: '/', first: '/', sep: '/', named: false, ifemp: '', allow: u },
+    { key: ';', first: ';', sep: ';', named: true, ifemp: '', allow: u },
+    { key: '?', first: '?', sep: '&', named: true, ifemp: '=', allow: u },
+    { key: '&', first: '&', sep: '&', named: true, ifemp: '=', allow: u },
+    { key: '#', first: '#', sep: ',', named: false, ifemp: '', allow: ur }
   ];
   const operator = operators.find(o => o.key === expression.charAt(1));
-  return operator ? operator : { key: null, first: '', allow: u };
+  return operator ? operator : defaultOperator;
 };
 
 // s = '{...}'
 const expression = (s: string, variables: any): string => {
-  const { key, first, allow } = parseOperator(s);
-  const varName = s.slice((key ? 2 : 1), s.length - 1);
-  const value = variables[varName];
-  return first + allow(value);
+  const { key, first, sep, named, ifemp, allow } = parseOperator(s);
+  const hasOperator = !!key;
+  const varNames = s.slice((hasOperator ? 2 : 1), s.length - 1).split(',');
+  return first + varNames.map(varName => {
+    const value = variables[varName];
+    const isEmpty = typeof value === 'undefined' ||
+      value === null ||
+      value.length === 0;
+    return (named ? allow(varName) + (isEmpty ? ifemp : '=') : '') + allow(value);
+  }).filter(s => s.length > 0).join(sep);
 };
 
 const literals = (s: string): string => {
