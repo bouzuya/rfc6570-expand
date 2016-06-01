924d5f1c9551a42eaac08b3b9782fb733920dad6
bouzuya <m@bouzuya.net>
2016-06-01 14:13:20 +0900

    refactor level 3 examples

--- test/level3.ts
+++ test/level3.ts
@@ -1,37 +1,15 @@
 import * as assert from 'power-assert';
 import { test } from 'eater/runner';
 import { init } from '../src/';
+import { testCases } from '../test/';
 
-test('level 3 examples', resolve => {
-  const variables = {
-    'var': 'value',
-    'hello': 'Hello World!',
-    'empty': '',
-    'path': '/foo/bar',
-    'x': '1024',
-    'y': '768'
-  };
-  const testCases = [
-    ['map?{x,y}', 'map?1024,768'],
-    ['{x,hello,y}', '1024,Hello%20World%21,768'],
-    ['{+x,hello,y}', '1024,Hello%20World!,768'],
-    ['{+path,x}/here', '/foo/bar,1024/here'],
-    ['{#x,hello,y}', '#1024,Hello%20World!,768'],
-    ['{#path,x}/here', '#/foo/bar,1024/here'],
-    ['X{.var}', 'X.value'],
-    ['X{.x,y}', 'X.1024.768'],
-    ['{/var}', '/value'],
-    ['{/var,x}/here', '/value/1024/here'],
-    ['{;x,y}', ';x=1024;y=768'],
-    ['{;x,y,empty}', ';x=1024;y=768;empty'],
-    ['{?x,y}', '?x=1024&y=768'],
-    ['{?x,y,empty}', '?x=1024&y=768&empty='],
-    ['?fixed=yes{&x}', '?fixed=yes&x=1024'],
-    ['{&x,y,empty}', '&x=1024&y=768&empty=']
-  ];
-  testCases.forEach(([template, uri]) => {
+const tests = testCases('spec-examples').filter(({ level }) => level === 3);
+tests.forEach(testCase => {
+  const { level, section, template, uris, variables } = testCase;
+  test(`${section} - ${level} - ${template}`, resolve => {
     const { expand } = init(template);
-    assert(expand(variables) === uri);
+    const uri = expand(variables);
+    assert(uris.some(u => u === uri), `${uri} / ${uris}`);
+    resolve();
   });
-  resolve();
 });
