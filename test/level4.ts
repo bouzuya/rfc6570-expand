4fb006427844552d2aa616c4658fa5ff4c2d0531
bouzuya <m@bouzuya.net>
2016-06-01 14:13:50 +0900

    add level 4 examples

---
+++ test/level4.ts
@@ -0,0 +1,15 @@
+import * as assert from 'power-assert';
+import { test } from 'eater/runner';
+import { init } from '../src/';
+import { testCases } from '../test/';
+
+const tests = testCases('spec-examples').filter(({ level }) => level === 4);
+tests.forEach(testCase => {
+  const { level, section, template, uris, variables } = testCase;
+  test(`${section} - ${level} - ${template}`, resolve => {
+    const { expand } = init(template);
+    const uri = expand(variables);
+    assert(uris.some(u => u === uri), `${uri} / ${uris}`);
+    resolve();
+  });
+});
