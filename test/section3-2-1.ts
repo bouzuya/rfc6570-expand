b1cb2677248aac39abaeaa74d127de73341bf620
bouzuya <m@bouzuya.net>
2016-06-01 23:50:13 +0900

    add spec-examples section 3.2.1

--- test/section3-2-1.ts
+++ test/section3-2-1.ts
@@ -3,10 +3,12 @@ import { test } from 'eater/runner';
 import { init } from '../src/';
 import { testCases } from '../test/';
 
-testCases('spec-examples').forEach(testCase => {
+const tests = testCases('spec-examples-by-section')
+  .sort((a, b) => a.section < a.section ? 1 : a.section === a.section ? 0 : -1)
+  .filter(({ section }) => !!section.match(/^3.2.1/));
+tests.forEach(testCase => {
   const { level, section, template, uris, variables } = testCase;
-  if (level !== 1 && level !== 2 && level !== 3) return;
-  test(`${section} - ${level} - ${template}`, resolve => {
+  test(`${section} - ${template}`, resolve => {
     const { expand } = init(template);
     const uri = expand(variables);
     assert(uris.some(u => u === uri), `${uri} / ${uris}`);
