f9aa34e638807c55e7ea39417e6e068919723936
bouzuya <m@bouzuya.net>
2016-06-03 21:07:34 +0900

    add spec-examples section 3.2.7

---
+++ test/section3-2-7.ts
@@ -0,0 +1,17 @@
+import * as assert from 'power-assert';
+import { test } from 'eater/runner';
+import { init } from '../src/';
+import { testCases } from '../test/';
+
+const tests = testCases('spec-examples-by-section')
+  .sort((a, b) => a.section < a.section ? 1 : a.section === a.section ? 0 : -1)
+  .filter(({ section }) => !!section.match(/^3.2.7/));
+tests.forEach(testCase => {
+  const { level, section, template, uris, variables } = testCase;
+  test(`${section} - ${template}`, resolve => {
+    const { expand } = init(template);
+    const uri = expand(variables);
+    assert(uris.some(u => u === uri), `${template} / ${uris} / ${uri}`);
+    resolve();
+  });
+});
